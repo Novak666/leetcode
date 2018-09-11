c6af98e05ff8cbab7438e05a17008571121dc7e8
Novak666 <huweiyu2008@163.com>
2018-09-12 07:57:30 +0800

    Tree

---
+++ Tree/104 Maximum Depth of Binary Tree/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root == NULL ? 0 : max(maxDepth(root -> left), maxDepth(root -> right)) + 1;
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
